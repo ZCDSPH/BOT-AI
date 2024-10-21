b1aa8d368b89c6838392f356df9213bc8247c785
ZCDSPH <toshchatbot@gmail.com>
2024-10-21 22:26:03 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
 
     const commands = commandFiles.map(file => {
       const command = require(path.join(commandsDir, file));
-      return `➤ ${command.name}\n  - ${command.description}\n  - Credits: ${command.author}`;
+      return `➤ ${command.name}\n  - ${command.description}\n  - Credits: 𝙼𝚊𝚛𝚓𝚑𝚞𝚗 𝙱𝚊𝚢𝚕𝚘𝚗`;
     });
 
     const totalCommands = commandFiles.length;
@@ -21,7 +21,7 @@ module.exports = {
     const end = start + pageSize;
     const paginatedCommands = commands.slice(start, end);
     
-    const helpMessage = `𝐓𝐎𝐒𝐇𝐈𝐀 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒: \n𝖳𝖮𝖳𝖠𝖫 𝖢𝖮𝖬𝖬𝖠𝖭𝖣𝖲: ${totalCommands} \n\n${paginatedCommands.join('\n\n')}\n\nPage ${page} of ${Math.ceil(totalCommands / pageSize)}\n\n𝐃𝐄𝐕𝐄𝐋𝐎𝐏𝐄𝐑 : 𝖬𝖠𝖱𝖩𝖧𝖴𝖭 𝖡𝖠𝖸𝖫𝖮𝖭`;
+    const helpMessage = `𝐓𝐎𝐒𝐇𝐈𝐀 𝐂𝐎𝐌𝐌𝐀𝐍𝐃𝐒: \nTotal Commands: ${totalCommands} \n\n${paginatedCommands.join('\n\n')}\n\nPage ${page} of ${Math.ceil(totalCommands / pageSize)}\n\n𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁 : 𝙼𝙰𝚁𝙹𝙷𝚄𝙽 𝙱𝙰𝚈𝙻𝙾𝙽`;
     
     sendMessage(senderId, { text: helpMessage }, pageAccessToken);
   }
