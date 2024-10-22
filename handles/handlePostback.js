e32ad4a17d15f990fdf64cac6833235b9e3eba0e
ZCDSPH <toshchatbot@gmail.com>
2024-10-22 22:13:13 +0800

    Update handlePostback.js

--- handles/handlePostback.js
+++ handles/handlePostback.js
@@ -1,11 +1,39 @@
 const { sendMessage } = require('./sendMessage');
+const path = require('path');
+const fs = require('fs');
 
-function handlePostback(event, pageAccessToken) {
+const commandsDir = path.join(__dirname, 'commands');
+const helpPath = path.join(commandsDir, 'help.js');
+
+let fetchCommandInfo, sendCommandButtons;
+
+if (fs.existsSync(helpPath)) {
+  const helpModule = require(helpPath);
+  fetchCommandInfo = helpModule.fetchCommandInfo;
+  sendCommandButtons = helpModule.sendCommandButtons;
+}
+
+async function handlePostback(event, pageAccessToken) {
   const senderId = event.sender.id;
   const payload = event.postback.payload;
 
-  // Send a message back to the sender
-  sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
+  if (payload.startsWith('HELP_')) {
+    const page = payload.replace('HELP_', '');
+    const helpCommand = require('./commands/help');
+    helpCommand.execute(senderId, [page], pageAccessToken, sendMessage);
+  } else if (payload.startsWith('COMMAND_')) {
+    const commandName = payload.replace('COMMAND_', '').toLowerCase();
+    const commandFilePath = path.join(commandsDir, `${commandName}.js`);
+    if (fs.existsSync(commandFilePath)) {
+      const commandModule = require(commandFilePath);
+      const commandInfo = `Command: ${commandModule.name}\nDescription: ${commandModule.description}\nCredit: ${commandModule.author}`;
+      await sendMessage(senderId, { text: commandInfo }, pageAccessToken);
+    } else {
+      await sendMessage(senderId, { text: `Error: Command '${commandName}' not found.` }, pageAccessToken);
+    }
+  } else {
+    await sendMessage(senderId, { text: `You sent a postback with payload: ${payload}` }, pageAccessToken);
+  }
 }
 
 module.exports = { handlePostback };
